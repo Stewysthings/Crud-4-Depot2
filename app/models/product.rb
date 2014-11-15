b4d613dbb4f8ce812a1aedb8d42a863d87763526
Wendy Stewart <cellardoor@hotmail.ca>
2014-11-15 08:38:31 -0800

    further changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	#checks that each of the named fields is present and full
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+       with:   %r{\.(gif|jpg|png)\Z}i,
+       message: 'must be a URL for GIF, JPG or PNG image.'
+    }
 end
